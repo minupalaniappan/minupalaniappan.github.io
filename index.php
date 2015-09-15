0a302923c8236a8e3b0f0fdb23e9052b7e9010ab
minupal <minupal6@gmail.com>
2015-09-14 20:35:11 -0700

    refreshed

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html> 
+
+<html>
+	<head>
+	<meta charset="UTF-8"> 
+	<meta name="viewport" content="width=device-ow width, initial-scale=1">
+	<meta name="description" content="Minu Palaniappan: dreamer who wants to see product and automation succeed">
+	<meta name="keywords" content="minupalaniappan, minupalaniappan design, minupalaniappan studio, minupalaniappanweb design and development, web design and development, responsive design, UI design, mobile app design, icon design, logo and identity systems, logo, print design, graphic, design, graphic design, graphic designer, web design, web designer, app design, app designer, interactive, interaction, user interface, UI, user experience, UX, typography, pattern, pattern designer, pattern design, textile, textile designer, textile design, front-end developer, developer, css, css3, html, html5, wordpress, blog design, blog development, California, Silicon Valley, Davis, portfolio, projects, angularfire, firebase, django, fullstack, sundar, palo alto, sunnyvale">
+	<title>Minu Palaniappan</title> 
+	<link rel="stylesheet" href="foundationcss/foundation.css"/>
+    <script src="foundationjs/vendor/modernizr.js"></script>
+	<link rel="stylesheet" href="foundationcss/app.css">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	</head>
+	<body>
+		<div class = "profile">
+			<img src="http://i.imgur.com/hlQ7tkp.jpg" class = "profile-pic">
+			<h3 style = "margin-top: 20px; color: #424242">Minu Palaniappan</h3>
+			<h4 style = "margin-top: 7px; color: #8A8A8A; font-size: 1.2em">Learning Developer & Product Hunter</h4>
+			<h4 style = "margin-top: 7px; color: #333333; font-size: 1.1em">Davis, CA</h4>
+		</div>
+		<div style = "width: 100%">
+			<ul class="tabs" data-tab>
+				<li class="tab-title active"><a style = "font-family: Proxima-SemiBold; height: 40px; padding-top: 8px; font-size: 1em; text-align: center; text-decoration: none; outline: none;" href="#projects" class = "tabbed">Projects</a></li>
+				<li class="tab-title"><a style = "font-family: Proxima-SemiBold; height: 40px; padding-top: 8px; font-size: 1em; text-align: center; text-decoration: none; outline: none;" href="#experiences">Experience</a></li>
+				<li class="tab-title"><a style = "font-family: Proxima-SemiBold; height: 40px; padding-top: 8px; font-size: 1em; text-align: center; text-decoration: none; outline: none;" href="#specializations">Specializations</a></li>
+				<li class="tab-title"><a style = "font-family: Proxima-SemiBold; height: 40px; padding-top: 8px; font-size: 1em; text-align: center; text-decoration: none; outline: none;" href="#about">About</a></li>
+			</ul>
+		</div>
+
+
+		<div class="tabs-content">
+			<div class="content active" id="projects" style = "width: 100%; margin: 0 auto; height: auto; background-color: white">	
+			</div>
+			<div class="content" id="experiences" style = "width: 100%px; margin: 0 auto; height: auto; background-color: white">
+			</div>
+			<div class="content" id="specializations" style = "width: 100%px; margin: 0 auto; height: auto; background-color: white">
+			</div>
+			<div class="content" id="about" style = "padding-top: 50px; text-align: center; width: 100%; margin: 0 auto; height: auto; background-color: white">
+				<h4 style = "color: #ADADAD">A little about myself</h4>
+				<h4 style = "color: #525252; font-size: 1.1em; width: 60%; margin: auto; margin-top: 10px; text-align: left; line-height: 2em">I'm currently a second year student at the University of California, Davis. I plan to major in Computer Science with a possible minor in technology management. I spent my first year at Davis building an awesome social network that had 500+ users post-launch. It was an experience that exposed me to team-management, product iteration and scaleable programming. I also enjoy attending hackathons and learning from the thousands of competitive hackers.</h4>
+				<h4 style = "color: #525252; font-size: 1.1em; width: 60%; margin: auto; margin-top: 10px; text-align: left; line-height: 2em">I'm also a huge basketball & tennis fan. When it comes to competing in an NBA fantasy league or talking about ATP's top 10 tennis players, I'm all ears. I also enjoy playing several sports - mostly basketball and tennis</h4>
+				<h4 style = "color: #525252; font-size: 1.1em; width: 60%; margin: auto; margin-top: 10px; text-align: left; line-height: 2em">In the future, I'd like to get involved with embedded system projects, vision-based technology or data-driven services.</h4>
+				<h3 style = "color: #525252; font-size: 1.1em; width: 60%; margin: auto; margin-top: 10px; text-align: left; line-height: 2em; text-decoration: underline">Resume</h3>
+				<h3 style = "color: #525252; font-size: 1.1em; width: 60%; margin: auto; text-align: left; line-height: 2em; text-decoration: underline">e: mspal@ucdavis.edu</h3>
+				<div style = "width: 60%; margin: auto; margin-top: 10px; text-align: left; ">
+					<h3 style = "color: #525252; font-size: 1.1em; margin-top: 10px; text-align: left;text-decoration: underline; display: inline-block; margin-right: 10px">Github</h3>
+					<h3 style = "color: #525252; font-size: 1.1em; margin-top: 10px; text-align: left;text-decoration: underline; display: inline-block; margin-right: 10px">LinkedIn</h3>
+					<h3 style = "color: #525252; font-size: 1.1em; margin-top: 10px; text-align: left;text-decoration: underline; display: inline-block; margin-right: 10px">Twitter</h3>
+				</div>
+			</div>
+		</div>
+
+
+
+
+		<script src="foundationjs/vendor/jquery.js"></script>
+		<script type="text/javascript" src = "scripts/search.js"></script>
+	    <script src="foundationjs/foundation.min.js"></script>
+		<script>
+		    $(document).foundation();
+		</script>
+	</body>
+</html>
+
