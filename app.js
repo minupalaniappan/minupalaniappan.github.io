12c02086e70932778dd4ee80ba843895a2cde2b8
Minu Palaniappan <minupalaniappan@campus-047-013.ucdavis.edu>
2018-03-14 15:47:06 -0700

    adding some nice sliders

---
+++ app.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal();
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.bio-img', {
+	origin: 'left',
+	distance: '30px'
+});
+sr.reveal('.text-content', {
+	origin: 'right',
+	distance: '20px'
+});
+sr.reveal('.about', {
+	origin: 'bottom',
+	distance: '40px'
+});
+sr.reveal('.work', {
+	origin: 'bottom',
+	distance: '30px'
+});
