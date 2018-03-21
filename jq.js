90ca898d7747295533e379d72bbcb485ecbd52d5
minupalaniappan <minupal6@gmail.com>
2018-03-20 18:05:47 -0700

    layout

--- jq.js
+++ jq.js
@@ -1,4 +1,21 @@
 $(document ).ready(function() {
+		console.log(isScrolledIntoView($("#2017")));
+
+	if (isScrolledIntoView($("#google")) || isScrolledIntoView($("#yelp"))) {
+		$("#2017_").addClass("scroll-select");
+		$("#2016_").removeClass("scroll-select");
+		$("#2010_").removeClass("scroll-select");
+	} else if (isScrolledIntoView($("#2016"))) {
+		$("#2017_").removeClass("scroll-select");
+		$("#2016_").addClass("scroll-select");
+		$("#2010_").removeClass("scroll-select");
+	} else if (isScrolledIntoView($("#2010"))) {
+		$("#2010_").addClass("scroll-select");
+		$("#2017_").removeClass("scroll-select");
+		$("#2016_").removeClass("scroll-select");
+	}
+
+
     var span = $('.work-story'),
     div = $('.wrap'),
     win = $(window),
@@ -6,19 +23,23 @@ $(document ).ready(function() {
 	$(document).scroll(compute).scroll();
 	$(window).resize(compute).resize();
   	
-  	function isScrolledIntoView(elem)
-	{
-	    var docViewTop = $(window).scrollTop();
-	    var docViewBottom = docViewTop + $(window).height();
+  	function isScrolledIntoView(el) {
+	  	//special bonus for those using jQuery
+	    if (typeof jQuery === "function" && el instanceof jQuery) {
+	        el = el[0];
+	    }
 
-	    var elemTop = $(elem).offset().top;
-	    var elemBottom = elemTop + $(elem).height();
-
-	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	    var rect = el.getBoundingClientRect();
+	    return (
+	        rect.top >= 0 &&
+	        rect.left >= 0 &&
+	        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+	        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+	    );
 	}
 
 	function compute() {
-		if (isScrolledIntoView($("#2017"))) {
+		if (isScrolledIntoView($("#google")) || isScrolledIntoView($("#yelp"))) {
 			$("#2017_").addClass("scroll-select");
 			$("#2016_").removeClass("scroll-select");
 			$("#2010_").removeClass("scroll-select");
